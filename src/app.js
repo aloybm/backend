4a0e165f4254db814e99437786d2a06d0eb54781
aloybm <manullangaloysius@gmail.com>
2022-12-11 01:40:58 +0700

    tamat cuy

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { auth, db } from '../firebase.js'
 // const express = require('express')
 const app = express()
 import cors from 'cors'
-import { addDoc, collection, getDoc, getDocs, onSnapshot, query, where, deleteDoc, doc } from 'firebase/firestore'
+import { setDoc, collection, getDocs, query, where, deleteDoc, doc, getDoc, updateDoc } from 'firebase/firestore'
 // const cors = require('cors')
 const port = 3000
 
@@ -34,15 +34,13 @@ app.get("/links", async(req, res)=>{
     const uid = req.query.uid
     try {
         let links = []
-        const q = query(collection(db, "link"), where ("UID", "==", uid ));
-        const unsubscribe = onSnapshot(q,
-            (querySnapshot) => {
-            querySnapshot.forEach((docSnap)=>{
+        const q = query(collection(db, "link"), where ("UID", "==", uid ))
+        const querySnapshot = await getDocs(q)
+        querySnapshot.forEach((docSnap)=>{
                 let id = docSnap.id
                 links.push({id, ...docSnap.data()})
-            })
-            res.send(links)
         })
+        res.send(links)
     }
         catch(err){
             console.log(err)
@@ -81,7 +79,6 @@ app.post("/login", async(req, res)=>{
             res.send(userCredential.user)
             console.log("login jalan", userCredential.user.uid)
             res.send(userCredential.user.id)
-            console.log('cek')
         }) 
         
     }
@@ -107,19 +104,25 @@ app.get("/logout", async(req, res) => {
 })
 
 app.post("/link", async(req, res) => {
+    const linkRef = doc(db, "link", req.body.newLink)
+    const docRef = await getDoc(linkRef)
     try {
-        await addDoc(collection(db, "link"), {
-            oldLink: req.body.oldLink,
-            newLink: req.body.newLink,
-            UID: req.body.uid,
-            viewCount: 0
-        })
-        console.log("Menambakan Link ke")
-        res.send("Menambakan Link")
+        if(!docRef.exists()){
+            await setDoc(doc(db, "link", req.body.newLink), {
+                oldLink: req.body.oldLink,
+                newLink: "127.0.0.1:5173/p/" + req.body.newLink,
+                UID: req.body.uid,
+                viewCount: 0
+            })
+            console.log("Menambakan Link")
+            res.send("Menambakan Link")
+        }
+        else throw "Custom Link Existed"
     }
     catch(err) {
         console.log("Gagal Menambakan Link")
-        res.send("Gagal Menambakan Link")
+        console.log(err)
+        res.send(err)
     }
 })
 
@@ -127,10 +130,57 @@ app.delete("/:id", async(req, res) => {
     const {id} = req.params
     try {
         await deleteDoc(doc(db, "link", id));
-        console.log('sukses')
+        res.send("Berhasil delete")
+        console.log('Delete')
     }
     catch(err) {
         console.log(err)
-        res.send("Gagal")
+        res.send("Gagal Delete")
+    }
+})
+
+app.post("/:id", async(req, res) => {
+    const {id} = req.params
+    const linkRef = doc(db, "link", req.body.newLink)
+    const docRef = await getDoc(linkRef)
+    try {
+        if(!docRef.exists()){
+            await setDoc(doc(db, "link", req.body.newLink), {
+                oldLink: req.body.oldLink,
+                newLink: "127.0.0.1:5173/p/" + req.body.newLink,
+                UID: req.body.uid,
+                viewCount: req.body.viewCount,
+            })
+            console.log("Update Link")
+            res.send("Update Link")
+        }
+        else throw "Custom Link Existed"
+    }
+    catch(err) {
+        console.log("Gagal Update Link")
+        console.log(err)
+        res.send(err)
+    }
+})
+
+app.get("/redirect", async(req, res)=>{
+    const id = req.query.id
+    const linkRef = doc(db, "link", id)
+    const docRef = await getDoc(linkRef)
+    try {
+        if(docRef.exists()) {
+            console.log(docRef.get('oldLink'))
+            console.log("Redirect Link")
+            res.send(docRef.get('oldLink'))
+            await updateDoc(linkRef, {
+                viewCount: docRef.get('viewCount') + 1
+            })
+        }
+
+        else throw "Redirect Link Not Existed"
+    }
+    catch(err){
+        console.log(err)
+        res.send(err)
     }
 })
